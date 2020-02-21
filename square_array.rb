094504f5f25c3beaceaa45890343a1b3a61ba9a6
Gilberto Ocampo <ocampo@gate.net>
2020-02-21 12:32:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,6 +7,10 @@
 def square_array(array)
   new_arr = []
   array.each { |number| new_arr << number ** 2 }
+<<<<<<< HEAD
   new_arr
+=======
+  square_array (new_arr)
+>>>>>>> e39e5f5d9a12c205c235e4c50cc3cf56b3687c15
 end
 
